78076db2afa226aed0c69da0abf6c104387808f9
VenerableCode <venerablecode@gmail.com>
2025-01-10 22:34:20 -0600

    Update KPatch.hpp

--- TheosMMTempKomaru/utils/KPatch.hpp
+++ TheosMMTempKomaru/utils/KPatch.hpp
@@ -12,14 +12,22 @@ https://github.com/VenerableCode/iOS-Theos-ModMenuTemp-NoJB
 
 class KomaruPatch {
 public:
+    static bool IsValidPointer(uintptr_t address) {
+        return address > 0x100000000 && address < 0x3000000000;
+    }
 
     static uintptr_t ReadMem(uintptr_t address) {
+        if (!IsValidPointer(address)) {
+            return 0;
+        }
         return *reinterpret_cast<uintptr_t*>(address);
     }
 
     template <typename T>
     static void WriteMem(uintptr_t address, const T& value) {
+        if (!IsValidPointer(address)) {
+            return;
+        }
         *reinterpret_cast<T*>(address) = value;
     }
-
 };
