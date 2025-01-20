5b8aa16170fedfc5927e512297d5c434015dcdc0
silentninjabee <91723045+silentninjabee@users.noreply.github.com>
2025-01-21 03:41:26 +1000

    Update BasicHacks.h

--- TheosMMTempKomaru/Source/BasicHacks.h
+++ TheosMMTempKomaru/Source/BasicHacks.h
@@ -3,9 +3,6 @@ IOS Theos Template Komaru
 Jailed (NoJB) Mod Menu Template for iOS Games
 By aq9
 https://github.com/VenerableCode/iOS-Theos-ModMenuTemp-NoJB
-*/
-
-
 
 #pragma once
 
@@ -19,7 +16,7 @@ public:
     }
 
     static bool IsValidPointer(long Offset);
-    static void* HacksThread(void* arg);
+    static void HacksThread();
 
     void Initialize();
 
@@ -28,3 +25,52 @@ private:
 };
 
 static BasicHacks& BasicCheats = BasicHacks::GetInstance();
+
+// BasicHacks.h
+#import <Foundation/Foundation.h>
+#include <mach-o/dyld.h>
+#include "KomaruPatch.h" // Include KomaruPatch header
+#include "../MenuLoad/Includes.h"
+*/
+// Define uintptr_t if not already defined
+#pragma once
+
+#ifndef uintptr_t
+#include <cstdint>
+using uintptr_t = std::uintptr_t;
+#endif
+
+
+namespace offsets {
+    extern constexpr uintptr_t OFFSET_GWorld;
+    extern constexpr uintptr_t OFFSET_OwningGameInstance;
+    extern constexpr uintptr_t OFFSET_LocalPlayers;
+    extern constexpr uintptr_t OFFSET_LocalPlayerController;
+    extern constexpr uintptr_t OFFSET_PlayerCameraManager;
+}
+
+class BasicHacks {
+public:
+    BasicHacks(const BasicHacks&) = delete;
+
+    static BasicHacks& GetInstance() {
+        static BasicHacks Instance;
+        return Instance;
+    }
+
+    void HacksThread();
+    void Initialize();
+
+private:
+    BasicHacks() { }
+};
+
+static BasicHacks& BasicCheats = BasicHacks::GetInstance();
+
+@interface GameLoop : NSObject
+
+- (void)start;
+- (void)stop;
+
+@end
+
